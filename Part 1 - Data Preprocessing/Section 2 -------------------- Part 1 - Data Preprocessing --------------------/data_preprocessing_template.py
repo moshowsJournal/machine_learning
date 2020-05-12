064fac957478e83dce67a72d321dac907595a3ff
Moshow <withyouamalive@gmail.com>
2020-05-12 08:17:02 +0100

    data has been preprocessed

--- Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/data_preprocessing_template.py
+++ Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/data_preprocessing_template.py
@@ -19,12 +19,12 @@ y = dataset.iloc[:,3].values
 
 #taking care of missing data
 
-"from sklearn.impute import SimpleImputer 
+"""from sklearn.impute import SimpleImputer 
 imputer = SimpleImputer(missing_values=np.nan,strategy='mean')
 imputer = imputer.fit(X[:,1:3])
-X[:,1:3] = imputer.transform(X[:,1:3])
+X[:,1:3] = imputer.transform(X[:,1:3])"""
 
-#Encoding categorical data
+"""#Encoding categorical data
 from sklearn.preprocessing import LabelEncoder,OneHotEncoder
 from sklearn.compose import ColumnTransformer
 labelencoder_X = LabelEncoder()
@@ -38,14 +38,14 @@ ct = ColumnTransformer(
 )
 X = ct.fit_transform(X)
 labelencoder_y = LabelEncoder()
-y = labelencoder_y.fit_transform(y)
+y = labelencoder_y.fit_transform(y) """
 
 # Splitting the dataset into Training set and Test set
 from sklearn.model_selection import train_test_split
 X_train, X_test,y_train,y_test = train_test_split(X,y,test_size=0.2,random_state = 0)
 
 #Feature Scaling
-from sklearn.preprocessing import StandardScaler
+"""from sklearn.preprocessing import StandardScaler
 sc_X = StandardScaler()
 X_train = sc_X.fit_transform(X_train)
-X_test = sc_X.fit_transform(X_test)
+X_test = sc_X.fit_transform(X_test)"""
